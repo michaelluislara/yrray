e7c546539c980ece482f889bab380577a56ee0fe
Michael <michael.lara@me.com>
2022-10-08 17:02:19 -0600

    added config

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+cubes = requests.get('https://www150.statcan.gc.ca/t1/wds/rest/getAllCubesListLite', stream=True)
+cubelist = cubes.json()
+cubelist = sorted(cubelist, key=lambda d: d['cubeTitleEn'])
+import os, re
 
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
-    
+    app.config['SECRET_KEY'] = 'your secret key'
     # a simple page that says hello
     @app.route('/')
     def indexpage():
@@ -21,17 +25,22 @@ def create_app(test_config=None):
         "base.html"
     )
     
-    @app.route('/dashboardlist')
-    def dashboardpage():
-        
-        cubes = requests.get('https://www150.statcan.gc.ca/t1/wds/rest/getAllCubesListLite', stream=True)
-        cubelist = cubes.json()
-        newlist = []
-        for x in range(0,len(cubelist)):
-            newlist.append(cubelist[x]['cubeTitleEn'])
+    @app.route('/dashboardlist', methods=('GET','POST'))
+    def dashboardpage(cubelist=cubelist):
+        if request.method == 'POST':
+            searchterm = request.form['searchfield']
+            # if not searchterm:
+            #     flash('A search term is required.')
+            # else:
+            newlist = {}
+            reobj = re.compile(searchterm)
+            for key in cubelist.keys():
+                if(reobj.search(key)):
+                    newlist[key] = newlist[key].values
+            return redirect("about.html", cubelist=newlist)
         return render_template(
         "dashboardlist.html",
-        newlist=newlist
+        cubelist=cubelist
     )
     
     @app.route('/about')
