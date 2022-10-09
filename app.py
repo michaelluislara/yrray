b8bc691c9fa4bd797993a99798483ba8acf4804d
Michael <michael.lara@me.com>
2022-10-08 19:57:23 -0600

    added a bunch of stuff

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import requests
 from flask import Flask, render_template, request, url_for, flash, redirect
+from forms import LoginForm, SearchForm
+from config import Config
 cubes = requests.get('https://www150.statcan.gc.ca/t1/wds/rest/getAllCubesListLite', stream=True)
 cubelist = cubes.json()
 cubelist = sorted(cubelist, key=lambda d: d['cubeTitleEn'])
@@ -11,7 +13,7 @@ import os, re
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your secret key'
+    app.config.from_object(Config)
     # a simple page that says hello
     @app.route('/')
     def indexpage():
@@ -27,17 +29,19 @@ def create_app(test_config=None):
     
     @app.route('/dashboardlist', methods=('GET','POST'))
     def dashboardpage(cubelist=cubelist):
-        if request.method == 'POST':
-            searchterm = request.form['searchfield']
-            # if not searchterm:
-            #     flash('A search term is required.')
-            # else:
-            newlist = {}
-            reobj = re.compile(searchterm)
-            for key in cubelist.keys():
-                if(reobj.search(key)):
-                    newlist[key] = newlist[key].values
-            return redirect("about.html", cubelist=newlist)
+        # if request.method == 'POST':
+        #     form = SearchForm()
+        #     searchterm = request.form['searchfield']
+        #     flash(f"you entered {searchterm}")
+        #     if not searchterm:
+        #         flash('A search term is required.')
+        #     else:
+        #         newlist = {}
+        #     reobj = re.compile(searchterm)
+        #     for key in cubelist.keys():
+        #         if(reobj.search(key)):
+        #             newlist[key] = newlist[key].values
+        #     return redirect("dashboardlist.html", cubelist=newlist)
         return render_template(
         "dashboardlist.html",
         cubelist=cubelist
@@ -48,5 +52,14 @@ def create_app(test_config=None):
         return render_template(
         "about.html"
     )
+        
+    @app.route('/login', methods=('GET','POST'))
+    def login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+            return redirect('/about')
+        return render_template('login.html', title='Sign In', form=form)
     
     return app
--- forms.py
+++ forms.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, RadioField
+from wtforms import StringField, SubmitField, TextAreaField, RadioField, PasswordField, BooleanField
 from wtforms.validators import DataRequired
 
-class searchform(FlaskForm):
-    
+class SearchForm(FlaskForm):
+    searchterm = StringField('Search Term')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
